278ce95375007fd275069826d4c6fc29a35ccef2
Lokhy87 <a.bolta@gmail.com>
2024-04-18 09:18:49 +0200

    Solo importante

--- utils.py
+++
@@ -1,8 +0,0 @@
-# Funcion reemplazo
-def sustitucion_nulos(row):
-    tournament = row['TOURNAMENT']
-    winner_prize = row['WINNER_PRIZE']
-    if pd.isnull(winner_prize) and any(t in tournament for t in tournament_means.key()):
-        return tournament_means(next(t for t in tournament_means.key() if t in tournament))
-    else:
-        return winner_prize
